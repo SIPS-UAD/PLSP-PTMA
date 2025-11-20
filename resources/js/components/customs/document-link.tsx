daada025c77101ed8d21cf2fdd710028d2c9d419
Tocacreamy <zekobaharudinfirdaus@gmail.com>
2025-11-20 21:31:47 +0700

    Refactor DocumentLink component styles and add LOGIN link to NavBar

--- resources/js/components/customs/document-link.tsx
+++ resources/js/components/customs/document-link.tsx
@@ -6,7 +6,7 @@ import {
 
 const DocumentLink = ({ document_link }: { document_link: string }) => {
     return (
-        <Link className="mx-2 rounded-sm bg-green-200 p-2 text-green-muhi flex items-center gap-2 hover:-translate-y-0.5 transition-all">
+        <Link className="mx-2 rounded-sm bg-slate-100 p-2 text-slate-500 flex items-center gap-2 hover:bg-slate-300  transition-all">
             <IoDocumentAttachOutline className='scale-120'/>
             <IoDocumentLockOutline className='scale-120 hidden'/>
             <h1 className='font-medium'>{document_link}</h1>
