aef034c86f987d9eeaf465499869863d98cd897c
VincentAlfa <vincent111232@gmail.com>
2025-12-10 20:48:23 +0700

    fix(migrations): correct column reference for thumbnail in posts and events tables

--- database/migrations/2025_12_08_071019_add_thumbnail_to_posts_table.php
+++ database/migrations/2025_12_08_071019_add_thumbnail_to_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->string('thumbnail')->nullable()->after('content');
+            $table->string('thumbnail')->nullable()->after('deskripsi');
         });
     }
 
