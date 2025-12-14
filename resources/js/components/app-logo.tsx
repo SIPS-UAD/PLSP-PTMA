f526640e44e67a37ec619b835255a477184da410
Nadya Prita Ramadhani <nadyapritaramadhani97@gmail.com>
2025-12-14 22:27:47 +0700

    Refactor code structure for improved readability and maintainability

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -7,7 +7,7 @@ export default function AppLogo() {
                 <AppLogoIcon className="size-5 fill-current text-white" />
             </div> */}
             <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <img src="/plsp-ptma-logo.svg" alt="PLSP PTMA" className="h-8 w-8 object-contain" />
+                <img src="/plsp-ptma.png" alt="PLSP PTMA" className="h-8 w-8 object-contain" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
