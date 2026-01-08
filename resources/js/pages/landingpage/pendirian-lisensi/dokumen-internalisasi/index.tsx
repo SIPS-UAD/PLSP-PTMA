c88d079e72e4cb1b7f874538efaa4bd61c57709a
Tocacreamy <zekobaharudinfirdaus@gmail.com>
2026-01-08 22:14:20 +0700

    feat(pendirian-lisensi): add internalization document page with layout

---
+++ resources/js/pages/landingpage/pendirian-lisensi/dokumen-internalisasi/index.tsx
@@ -0,0 +1,13 @@
+import Layout from '../../../../layouts/landingpage/landingpage-layout';
+
+const index = () => {
+  return (
+    <Layout>
+      <section className="container h-svh pt-16">
+        <h1 className="text-3xl font-bold underline">Dokumen Internalisasi</h1>
+      </section>
+    </Layout>
+  );
+};
+
+export default index;
